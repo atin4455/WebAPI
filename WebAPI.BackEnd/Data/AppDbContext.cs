f2b233b47bcc22f7b5ca9475a8dbd0a362bcd0d8
祖程 <atinzulin@hotmail.com>
2026-06-15 14:56:19 +0800

    新增後端及註冊DB(使用InMemory 資料庫)

---
+++ WebAPI.BackEnd/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.BackEnd.Models;
+
+namespace WebAPI.BackEnd.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Todo> Todos { get; set; }
+}
--- WebAPI.BackEnd/Program.cs
+++ WebAPI.BackEnd/Program.cs
@@ -1,23 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.BackEnd.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. 註冊 InMemory 資料庫
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("TodoListDb"));
+
+// 2. 開放 CORS 權限給前端 React
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // 這是前端 Vite 的預設 Port
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// 3. 這是最新 .NET 的 OpenAPI 註冊方式 (取代舊的 AddSwaggerGen)
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 4. 這是最新 .NET 開啟 OpenAPI 路由的方式 (取代舊的 UseSwagger)
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); // 這會產生一個 json 檔案記錄你的 API 長相
 }
 
-app.UseHttpsRedirection();
+// 5. CORS 記得放在 Authorization 之前
+app.UseCors("AllowReact");
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
